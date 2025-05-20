b03792d63bf3e148700202d0fa4331922094faae
NgZiXin <ngzixin02@gmail.com>
2025-05-20 23:41:06 +0800

    Create ThemeContext

---
+++ app/context/ThemeContext.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { createContext, useContext, useState, ReactNode } from "react";
+
+type Theme = "light" | "dark";
+
+interface ThemeContextType {
+  theme: Theme;
+  toggleTheme: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+const useTheme = () => {
+  const context = useContext(ThemeContext);
+  if (!context) throw new Error("useTheme must be used within ThemeProvider");
+  return context;
+};
+
+function ThemeProvider({ children }: { children: ReactNode }) {
+  const [theme, setTheme] = useState<Theme>("light");
+
+  const toggleTheme = () => setTheme(prev => (prev === "light" ? "dark" : "light"));
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+        {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export {ThemeProvider, useTheme}

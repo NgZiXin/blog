296941bf660cf973cf12263a5d0958b991600b89
NgZiXin <ngzixin02@gmail.com>
2025-05-29 02:17:26 +0800

    Fix Eslint issues

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -16,25 +16,25 @@ export default function About() {
         <h1 className="text-3xl font-bold p-6">Hi! I am ZiXin</h1>
       <div className="text-base text-justify font-sans leading-relaxed text-gray-700 dark:text-gray-300 space-y-4">
         <p>
-          Hi! I'm an incoming third-year student at the National University of
+          Hi! I&apos;m an incoming third-year student at the National University of
           Singapore, and I have a deep passion for endurance sports — especially
           running, cycling, and swimming. I love being outdoors and rarely stay
-          still when I travel. Whether I'm exploring a city or hiking up a
-          mountain, I'm always chasing a little adventure. In recent years, I've
+          still when I travel. Whether I&apos;m exploring a city or hiking up a
+          mountain, I&apos;m always chasing a little adventure. In recent years, I&apos;ve
           really grown to love travel. Southeast Asia has been my playground,
           with unforgettable experiences in Indonesia, Malaysia, and Thailand. I
           find joy in discovering hidden trails, scenic peaks, and the stories
           each place holds.
         </p>
         <p>
-          This summer, I'll be heading to Hong Kong for an internship with
-          Expando World Limited, and shortly after, I'll begin my semester
-          exchange in Vancouver at Simon Fraser University. I'm incredibly
+          This summer, I&apos;ll be heading to Hong Kong for an internship with
+          Expando World Limited, and shortly after, I&apos;ll begin my semester
+          exchange in Vancouver at Simon Fraser University. I&apos;m incredibly
           excited to meet new people, immerse myself in different cultures, and
           keep exploring new places.
         </p>
         <p>
-          This blog is where I'll be sharing some of my favorite memories and
+          This blog is where I&apos;ll be sharing some of my favorite memories and
           stories from my travels! :D
         </p>
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
       <main className="flex flex-col items-center w-full p-8 sm:p-12">
         {/* <Clock /> */}
         <h1 className="text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-950 dark:from-blue-500 to-blue-700 dark:to-blue-750 uppercase mb-6">
-          ZiXin's Blog
+          ZiXin&apos;s Blog
         </h1>
         <Carousel posts={posts} />
       </main>
